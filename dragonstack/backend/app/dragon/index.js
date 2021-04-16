e6be9aaa2caa63e05a9a76ae13f88c8d02b0560d
JONATHAN <jonfkern@gmail.com>
2021-04-16 08:41:56 -0400

    section 4, lesson 53: verify dragon trait storage

--- dragonstack/backend/app/dragon/index.js
+++ dragonstack/backend/app/dragon/index.js
@@ -1,41 +1,36 @@
-// use scream case that don't change values for the application
 const TRAITS = require ('../../data/traits.json');
 
 const DEFAULT_PROPERTIES = {
     nickname: 'unnamed',
     generationId: undefined,
-    get birthdate() { // getter method will run new date at time of execution
+    get birthdate() { 
         return new Date()
     },
     get randomTraits() {
-        const traits = []; // setter, set to array
+        const traits = []; 
 
-        // every trait, we are going to set it to our own type and values
         TRAITS.forEach(TRAIT => {
             const traitType = TRAIT.type;
             const traitValues = TRAIT.values;
 
-            // pick random trait from the value array that we're getting from individual trait object
             const traitValue = traitValues[
                 Math.floor(Math.random() * traitValues.length)
             ]
 
-            // push object to "traits" array
             traits.push({ traitType, traitValue })
         });
 
-        return traits; // return the traits that have been built as part of the get randomTraits computed property
+        return traits;
     }
 }; 
 
 class Dragon {
-    // accept incoming object with keys
-    constructor({ birthdate, nickname, traits, generationId } = {}) { // set to empty object in case no properties provided
-        this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate; // use defaults if no values provide
+    constructor({ birthdate, nickname, traits, generationId } = {}) {
+        this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
         this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
         this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
         this.generationId = generationId || DEFAULT_PROPERTIES.generationId;
     }
 }
 
-module.exports = Dragon; // Export to be available in other files
+module.exports = Dragon;
