c60d6ce1f7740076cf6a6eded6bd6851d829c12d
JONATHAN <jonfkern@gmail.com>
2021-08-10 08:31:26 -0400

    Section 65: React State and Generation Fetch | Part 1

--- dragonstack/frontend/dist/src.e31bb0bc.js
+++ dragonstack/frontend/dist/src.e31bb0bc.js
@@ -18466,18 +18466,35 @@ var Generation = /*#__PURE__*/function (_Component) {
   var _super = _createSuper(Generation);
 
   function Generation() {
+    var _temp, _this;
+
     _classCallCheck(this, Generation);
 
-    return _super.apply(this, arguments);
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _possibleConstructorReturn(_this, (_temp = _this = _super.call.apply(_super, [this].concat(args)), _this.state = {
+      generation: {
+        generationId: 999,
+        expiration: '2020-05-01'
+      }
+    }, _this.fetchGeneration = function () {
+      fetch('http://localhost:3000/generation').then(function (response) {
+        return console.log('response', response);
+      });
+    }, _temp));
   }
 
   _createClass(Generation, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.fetchGeneration();
+    }
+  }, {
     key: "render",
     value: function render() {
-      var generation = {
-        generationId: 999,
-        expiration: '2020-05-01'
-      };
+      var generation = this.state.generation;
       return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h3", null, "Generation ", generation.generationId, ". Expires on:"), /*#__PURE__*/_react.default.createElement("h4", null, new Date(generation.expiration).toString()));
     }
   }]);
@@ -18527,7 +18544,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50525" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54339" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
