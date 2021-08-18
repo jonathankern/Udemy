9a53fe1deb0129996d064cb7b2ecc250b4625fc6
JONATHAN <jonfkern@gmail.com>
2021-08-18 08:29:27 -0400

    Lesson 66: Backend Interlude: Cross-Origin Resource Sharing

--- dragonstack/backend/app/index.js
+++ dragonstack/backend/app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const GenerationEngine = require('./generation/engine');
 const dragonRouter = require('./api/dragon'); // import dragon router
 const generationRouter = require('./api/generation'); //  import generation router
@@ -8,6 +9,7 @@ const engine = new GenerationEngine();
 
 app.locals.engine = engine;
 
+app.use(cors({ origin: 'http://localhost:1234' })); // set backend to have the same origin as frontend
 app.use('/dragon', dragonRouter); // (subroute, router instance) attach all routes that we define on the /dragon subroute
 app.use('/generation', generationRouter);
 
