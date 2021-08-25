2275146db344a602306cffc9476ffa06ae9526f8
JONATHAN <jonfkern@gmail.com>
2021-08-25 08:48:17 -0400

    Lesson 73: New Dragon Button and React Bootstrap

--- dragonstack/frontend/src/components/Dragon.js
+++ dragonstack/frontend/src/components/Dragon.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 import DragonAvatar from './DragonAvatar';
 
 const DEFAULT_DRAGON = { 
@@ -29,9 +30,16 @@ class Dragon extends Component {
 
     render() {
         return (
-            <DragonAvatar
-                dragon={this.state.dragon}
-            />
+            <div>
+                <Button
+                    onClick={this.fetchDragon}
+                >
+                    New Dragon
+                </Button>
+                <DragonAvatar
+                    dragon={this.state.dragon}
+                />
+            </div>
         )
     }
 }
