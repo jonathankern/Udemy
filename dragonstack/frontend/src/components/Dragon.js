e39af51d96c484c7f667644555352cce16333f0f
JONATHAN <jonfkern@gmail.com>
2021-08-25 08:26:57 -0400

    Lesson 72: Dragon Avatar Component and React Props

--- dragonstack/frontend/src/components/Dragon.js
+++ dragonstack/frontend/src/components/Dragon.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DragonAvatar from './DragonAvatar';
 
 const DEFAULT_DRAGON = { 
     dragonId: '',
@@ -27,15 +28,10 @@ class Dragon extends Component {
     };
 
     render() {
-        const { generationId, dragonId, traits } = this.state.dragon;
-
         return (
-            <div>
-                <span>G {generationId}.</span>
-                <span>I {dragonId}. </span>
-
-                { traits.map(trait => trait.traitValue).join(', ') }
-            </div>
+            <DragonAvatar
+                dragon={this.state.dragon}
+            />
         )
     }
 }
