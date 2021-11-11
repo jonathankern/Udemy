0624319731e335ecebf615359b340b9af66222d0
JONATHAN <jonfkern@gmail.com>
2021-11-11 08:48:35 -0500

    Lesson 99, Account API and New Account Request

--- dragonstack/backend/app/index.js
+++ dragonstack/backend/app/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const GenerationEngine = require('./generation/engine');
 const dragonRouter = require('./api/dragon'); // import dragon router
 const generationRouter = require('./api/generation'); //  import generation router
+const accountRouter = require('./api/account');
 
 const app = express(); // set up server
 const engine = new GenerationEngine();
@@ -10,6 +12,9 @@ const engine = new GenerationEngine();
 app.locals.engine = engine;
 
 app.use(cors({ origin: 'http://localhost:1234' })); // set backend to have the same origin as frontend
+app.use(bodyParser.json());
+
+app.use('/account', accountRouter);
 app.use('/dragon', dragonRouter); // (subroute, router instance) attach all routes that we define on the /dragon subroute
 app.use('/generation', generationRouter);
 
