8ebd41633bd93aab0b61e079c22e53d38d77b273
JONATHAN <jonfkern@gmail.com>
2021-12-05 13:18:24 -0500

    Lesson 123: Log Out and Account Reducer

--- dragonstack/frontend/dist/src.e31bb0bc.js
+++ dragonstack/frontend/dist/src.e31bb0bc.js
@@ -21674,7 +21674,8 @@ exports.DRAGON = DRAGON;
 var ACCOUNT = {
   FETCH: 'ACCOUNT_FETCH',
   FETCH_ERROR: 'ACCOUNT_FETCH_ERROR',
-  FETCH_SUCCESS: 'ACCOUNT_FETCH_SUCCESS'
+  FETCH_SUCCESS: 'ACCOUNT_FETCH_SUCCESS',
+  FETCH_LOGOUT_SUCCESS: 'ACCOUNT_FETCH_LOGOUT_SUCCESS'
 };
 exports.ACCOUNT = ACCOUNT;
 },{}],"reducers/fetchStates.js":[function(require,module,exports) {
@@ -21844,6 +21845,13 @@ var account = function account() {
         status: _fetchStates.default.success,
         message: action.message,
         loggedIn: true
+      });
+
+    case _types.ACCOUNT.FETCH_LOGOUT_SUCCESS:
+      return _objectSpread(_objectSpread({}, state), {}, {
+        status: _fetchStates.default.success,
+        message: action.message,
+        loggedIn: false
       });
 
     default:
--- dragonstack/frontend/src/reducers/account.js
+++ dragonstack/frontend/src/reducers/account.js
@@ -25,6 +25,14 @@ const account = (state = DEFAULT_ACCOUNT, action) => {
                 message: action.message,
                 loggedIn: true
             };
+        
+        case ACCOUNT.FETCH_LOGOUT_SUCCESS:
+            return {
+                ...state,
+                status: fetchStates.success,
+                message: action.message,
+                loggedIn: false
+            }
 
         default:
             return state;
